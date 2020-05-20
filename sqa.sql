97e421f7013d8fec5fdc05dce72efd4093671530
kens3dola <58432333+kens3dola@users.noreply.github.com>
2020-05-20 20:28:17 +0700

    Update sqa.sql

--- sqa.sql
+++ sqa.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 21, 2020 at 09:32 AM
+-- Generation Time: May 10, 2020 at 01:56 PM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.3
 
@@ -21,20 +21,23 @@ SET time_zone = "+00:00";
 --
 -- Database: `sqa`
 --
-CREATE DATABASE sqa;
-USE sqa;
+CREATE DATABASE IF NOT EXISTS `sqa` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `sqa`;
+
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `account`
 --
 
-CREATE TABLE `account` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `account`;
+CREATE TABLE IF NOT EXISTS `account` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(24) NOT NULL,
   `password` varchar(24) NOT NULL,
-  `role` enum('admin','user') NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `role` enum('admin','staff') NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `account`
@@ -42,7 +45,8 @@ CREATE TABLE `account` (
 
 INSERT INTO `account` (`id`, `username`, `password`, `role`) VALUES
 (1, 'admin', 'admin', 'admin'),
-(2, 'staff', 'staff', 'user');
+(2, 'staff', 'staff', 'staff'),
+(4, 'quang', 'sang', 'staff');
 
 -- --------------------------------------------------------
 
@@ -50,8 +54,10 @@ INSERT INTO `account` (`id`, `username`, `password`, `role`) VALUES
 -- Table structure for table `admin`
 --
 
-CREATE TABLE `admin` (
-  `account_id` int(11) DEFAULT NULL
+DROP TABLE IF EXISTS `admin`;
+CREATE TABLE IF NOT EXISTS `admin` (
+  `account_id` int(11) DEFAULT NULL,
+  KEY `fk` (`account_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -67,19 +73,23 @@ INSERT INTO `admin` (`account_id`) VALUES
 -- Table structure for table `item`
 --
 
-CREATE TABLE `item` (
-  `id` int(11) NOT NULL,
-  `name` enum('popcorn','redbull') NOT NULL,
+DROP TABLE IF EXISTS `item`;
+CREATE TABLE IF NOT EXISTS `item` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` enum('popcorn','coca') NOT NULL,
   `quantity` int(11) NOT NULL,
-  `order_id` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `order_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk1` (`order_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `item`
 --
 
 INSERT INTO `item` (`id`, `name`, `quantity`, `order_id`) VALUES
-(1, 'popcorn', 2, 1);
+(23, 'popcorn', 1, 32),
+(24, 'coca', 2, 32);
 
 -- --------------------------------------------------------
 
@@ -87,19 +97,24 @@ INSERT INTO `item` (`id`, `name`, `quantity`, `order_id`) VALUES
 -- Table structure for table `movie`
 --
 
-CREATE TABLE `movie` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `movie`;
+CREATE TABLE IF NOT EXISTS `movie` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
   `image` text NOT NULL,
-  `price` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `price` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `movie`
 --
 
 INSERT INTO `movie` (`id`, `name`, `image`, `price`) VALUES
-(1, 'Balika Vadhu', 'https://upload.wikimedia.org/wikipedia/vi/2/29/Balika_Vadhu.png', 5);
+(1, 'Balika Vadhu', 'BalikaVadhu.png', 5),
+(2, 'titanic', 'titanic.jpg', 10),
+(4, 'Bahubali', 'bahubali.jpg', 10),
+(6, 'Sex education', 'sexeducation.jpg', 10);
 
 -- --------------------------------------------------------
 
@@ -107,38 +122,23 @@ INSERT INTO `movie` (`id`, `name`, `image`, `price`) VALUES
 -- Table structure for table `order`
 --
 
-CREATE TABLE `order` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `order`;
+CREATE TABLE IF NOT EXISTS `order` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `staff_id` int(11) DEFAULT NULL,
-  `status` enum('confirmed','dennied') NOT NULL,
-  `date` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `customer` varchar(100) NOT NULL,
+  `date` date NOT NULL,
+  `price` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk3` (`staff_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `order`
 --
 
-INSERT INTO `order` (`id`, `staff_id`, `status`, `date`) VALUES
-(1, 1, 'confirmed', '2020-04-19');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `room`
---
-
-CREATE TABLE `room` (
-  `id` int(11) NOT NULL,
-  `name` enum('S0','S1','S2','S3','S4','S5') NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `room`
---
-
-INSERT INTO `room` (`id`, `name`) VALUES
-(1, 'S0'),
-(2, 'S1');
+INSERT INTO `order` (`id`, `staff_id`, `customer`, `date`, `price`) VALUES
+(32, 1, 'phamsang', '2020-05-10', 9);
 
 -- --------------------------------------------------------
 
@@ -146,10 +146,12 @@ INSERT INTO `room` (`id`, `name`) VALUES
 -- Table structure for table `schedule`
 --
 
-CREATE TABLE `schedule` (
-  `id` int(11) NOT NULL,
-  `shift` enum('1','2','3','4') NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+DROP TABLE IF EXISTS `schedule`;
+CREATE TABLE IF NOT EXISTS `schedule` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `shift` enum('1','2','3','4') NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `schedule`
@@ -167,22 +169,23 @@ INSERT INTO `schedule` (`id`, `shift`) VALUES
 -- Table structure for table `seat`
 --
 
-CREATE TABLE `seat` (
-  `id` int(11) NOT NULL,
-  `type` enum('S','A','B') NOT NULL,
+DROP TABLE IF EXISTS `seat`;
+CREATE TABLE IF NOT EXISTS `seat` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` enum('S1','S2','S3','S4','S5','S6','S7','S8','S9','A1','A2','A3','A4','A5','A6','A7','A8','A9','B1','B2','B3','B4','B5','B6','B7','B8','B9') NOT NULL,
   `price` int(11) NOT NULL,
-  `status` enum('Available','Unavailable') NOT NULL,
-  `room_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `room` enum('A','B','C','D') NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `seat`
 --
 
-INSERT INTO `seat` (`id`, `type`, `price`, `status`, `room_id`) VALUES
-(1, 'S', 3, 'Available', 1),
-(2, 'A', 2, 'Available', 1),
-(3, 'B', 1, 'Available', 1);
+INSERT INTO `seat` (`id`, `name`, `price`, `room`) VALUES
+(1, '', 3, 'A'),
+(2, '', 2, 'A'),
+(3, '', 1, 'A');
 
 -- --------------------------------------------------------
 
@@ -190,18 +193,26 @@ INSERT INTO `seat` (`id`, `type`, `price`, `status`, `room_id`) VALUES
 -- Table structure for table `showtime`
 --
 
-CREATE TABLE `showtime` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `showtime`;
+CREATE TABLE IF NOT EXISTS `showtime` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `schedule_id` int(11) NOT NULL,
-  `movie_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `movie_id` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `room` enum('A','B','C','D') NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk4` (`movie_id`),
+  KEY `fk5` (`schedule_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `showtime`
 --
 
-INSERT INTO `showtime` (`id`, `schedule_id`, `movie_id`) VALUES
-(1, 1, 1);
+INSERT INTO `showtime` (`id`, `schedule_id`, `movie_id`, `date`, `room`) VALUES
+(1, 1, 1, '2020-05-13', 'A'),
+(5, 3, 4, '2020-05-13', 'A'),
+(6, 3, 4, '2020-05-13', 'C');
 
 -- --------------------------------------------------------
 
@@ -209,22 +220,25 @@ INSERT INTO `showtime` (`id`, `schedule_id`, `movie_id`) VALUES
 -- Table structure for table `staff`
 --
 
-CREATE TABLE `staff` (
-  `id` int(11) NOT NULL,
-  `name` varchar(50) NOT NULL,
+DROP TABLE IF EXISTS `staff`;
+CREATE TABLE IF NOT EXISTS `staff` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) DEFAULT NULL,
   `age` int(11) DEFAULT NULL,
   `gender` enum('male','female') DEFAULT NULL,
   `email` varchar(50) DEFAULT NULL,
   `phone` varchar(13) DEFAULT NULL,
-  `account_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `account_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk6` (`account_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `staff`
 --
 
 INSERT INTO `staff` (`id`, `name`, `age`, `gender`, `email`, `phone`, `account_id`) VALUES
-(1, 'dang cap', 17, 'female', 'dangcap502@gmail.com', '01686868668', 2);
+(1, 'Pham', 17, 'male', 'Sang', '0889683268', 2);
 
 -- --------------------------------------------------------
 
@@ -232,164 +246,27 @@ INSERT INTO `staff` (`id`, `name`, `age`, `gender`, `email`, `phone`, `account_i
 -- Table structure for table `ticket`
 --
 
-CREATE TABLE `ticket` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `ticket`;
+CREATE TABLE IF NOT EXISTS `ticket` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `order_id` int(11) NOT NULL,
   `showtime_id` int(11) NOT NULL,
-  `seat_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `seat_id` int(11) NOT NULL,
+  `price` int(11) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`),
+  KEY `fk7` (`order_id`),
+  KEY `fk8` (`showtime_id`),
+  KEY `fk9` (`seat_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=43 DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `ticket`
 --
 
-INSERT INTO `ticket` (`id`, `order_id`, `showtime_id`, `seat_id`) VALUES
-(1, 1, 1, 1),
-(2, 1, 1, 2),
-(3, 1, 1, 3);
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `account`
---
-ALTER TABLE `account`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `admin`
---
-ALTER TABLE `admin`
-  ADD KEY `fk` (`account_id`);
-
---
--- Indexes for table `item`
---
-ALTER TABLE `item`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `fk1` (`order_id`);
-
---
--- Indexes for table `movie`
---
-ALTER TABLE `movie`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `order`
---
-ALTER TABLE `order`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `fk3` (`staff_id`);
-
---
--- Indexes for table `room`
---
-ALTER TABLE `room`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `schedule`
---
-ALTER TABLE `schedule`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `seat`
---
-ALTER TABLE `seat`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `fk10` (`room_id`);
-
---
--- Indexes for table `showtime`
---
-ALTER TABLE `showtime`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `fk4` (`movie_id`),
-  ADD KEY `fk5` (`schedule_id`);
-
---
--- Indexes for table `staff`
---
-ALTER TABLE `staff`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `fk6` (`account_id`);
-
---
--- Indexes for table `ticket`
---
-ALTER TABLE `ticket`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `fk7` (`order_id`),
-  ADD KEY `fk8` (`showtime_id`),
-  ADD KEY `fk9` (`seat_id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `account`
---
-ALTER TABLE `account`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT for table `item`
---
-ALTER TABLE `item`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT for table `movie`
---
-ALTER TABLE `movie`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT for table `order`
---
-ALTER TABLE `order`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT for table `room`
---
-ALTER TABLE `room`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT for table `schedule`
---
-ALTER TABLE `schedule`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- AUTO_INCREMENT for table `seat`
---
-ALTER TABLE `seat`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT for table `showtime`
---
-ALTER TABLE `showtime`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT for table `staff`
---
-ALTER TABLE `staff`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT for table `ticket`
---
-ALTER TABLE `ticket`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+INSERT INTO `ticket` (`id`, `order_id`, `showtime_id`, `seat_id`, `price`) VALUES
+(40, 32, 1, 4, 3),
+(41, 32, 1, 14, 2),
+(42, 32, 1, 24, 1);
 
 --
 -- Constraints for dumped tables
@@ -414,12 +291,6 @@ ALTER TABLE `order`
   ADD CONSTRAINT `fk3` FOREIGN KEY (`staff_id`) REFERENCES `staff` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Constraints for table `seat`
---
-ALTER TABLE `seat`
-  ADD CONSTRAINT `fk10` FOREIGN KEY (`room_id`) REFERENCES `room` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
 -- Constraints for table `showtime`
 --
 ALTER TABLE `showtime`
@@ -437,8 +308,7 @@ ALTER TABLE `staff`
 --
 ALTER TABLE `ticket`
   ADD CONSTRAINT `fk7` FOREIGN KEY (`order_id`) REFERENCES `order` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `fk8` FOREIGN KEY (`showtime_id`) REFERENCES `showtime` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `fk9` FOREIGN KEY (`seat_id`) REFERENCES `seat` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `fk8` FOREIGN KEY (`showtime_id`) REFERENCES `showtime` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
